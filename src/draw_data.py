133f44043a9e603aeca4020fec17bda1d0f9bd1e
Agry88 <rgok307085@gmail.com>
2023-06-03 15:48:36 +0800

    Fix: typo.

--- src/draw_data.py
+++ src/draw_data.py
@@ -10,7 +10,7 @@ plt.rcParams['axes.unicode_minus'] = False
 
 # 各候選人聲量長條圖
 def drawPrincipalVolumeBar(dataframe: pd.DataFrame):
-    dataframe['principal'].value_counts().plot(kind='bar',title='各品牌聲量長條圖', xlabel='品牌', ylabel='聲量')
+    dataframe['principal'].value_counts().plot(kind='bar',title='各候選人聲量長條圖', xlabel='候選人', ylabel='聲量')
     plt.show()
 
 
@@ -26,7 +26,7 @@ def drawPrincipalVolumeLine(dataframe: pd.DataFrame):
     dataframe[dataframe['principal'] == '賴清德']['time'].value_counts().sort_index().plot(kind='line', color='green')
 
     plt.legend(['柯文哲', '侯友宜', '賴清德'])
-    plt.title('各品牌聲量折線圖(趨勢圖)')
+    plt.title('各候選人聲量折線圖(趨勢圖)')
     plt.xlabel('日期')
     plt.ylabel('聲量')
     plt.show()
@@ -34,7 +34,7 @@ def drawPrincipalVolumeLine(dataframe: pd.DataFrame):
 # 各候選人情緒長條圖
 def drawPrincipalSentimentBar(dataframe: pd.DataFrame):
     principleSentiments = dataframe.groupby(['principal'])['sentiment'].mean()
-    principleSentiments.plot(kind='bar',title='各品牌情緒長條圖', xlabel='品牌', ylabel='情緒')
+    principleSentiments.plot(kind='bar',title='各候選人情緒長條圖', xlabel='候選人', ylabel='情緒')
     plt.show()
 
 # 各候選人情緒折線圖(趨勢圖)
@@ -49,7 +49,7 @@ def drawPrincipalSentimentLine(dataframe: pd.DataFrame):
     dataframe[dataframe['principal'] == '賴清德'].groupby(['time'])['sentiment'].mean().sort_index().plot(kind='line', color='green')
 
     plt.legend(['柯文哲', '侯友宜', '賴清德'])
-    plt.title('各品牌情緒折線圖(趨勢圖)')
+    plt.title('各候選人情緒折線圖(趨勢圖)')
     plt.xlabel('日期')
     plt.ylabel('情緒')
     plt.show()
@@ -78,7 +78,7 @@ def drawPrincipalVolumeAndSentimentScatter(dataframe: pd.DataFrame):
     plt.scatter(principleVolumes, principleSentiments)
     for i in range(len(principleVolumes)):
         plt.annotate(principleVolumes.index[i], (principleVolumes[i], principleSentiments[i]))
-    plt.title('各品牌聲量與情緒比較圖(象限圖)')
+    plt.title('各候選人聲量與情緒比較圖(象限圖)')
     plt.xlabel('聲量')
     plt.ylabel('情緒')
     plt.show()
